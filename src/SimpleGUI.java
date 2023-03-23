2da677f01c1b0182fcc523a7c8e6f0f7c95bab91
1E10 Laptop <sample@bths.edu>
2023-03-23 12:34:27 -0400

    j

--- src/SimpleGUI.java
+++ src/SimpleGUI.java
@@ -1,2 +1,122 @@
-package PACKAGE_NAME;public class SimpleGUI {
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import java.awt.Font;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SimpleGUI extends JFrame implements ActionListener {
+    private JLabel welcomeLabel;
+    public SimpleGUI() {
+        super("Frame title");
+        init();
+    }
+
+    private void init() {
+        // setting up the frame
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 400);
+        setLocation(300, 50);
+
+        // create the MenuBar and menu components
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu1 = new JMenu("File");
+        JMenuItem menuItem1 = new JMenuItem("Open");
+        JMenuItem menuItem2 = new JMenuItem("Save as");
+        menu1.add(menuItem1);
+        menu1.add(menuItem2);
+        JMenu menu2 = new JMenu("Help");
+
+        // add "File" and "Help" menus to the MenuBar
+        menuBar.add(menu1);
+        menuBar.add(menu2);
+
+        // create the big text area located in the middle
+        JTextArea textArea = new JTextArea();
+
+        // create welcome label
+        welcomeLabel = new JLabel("Welcome to my GUI!");
+        welcomeLabel.setFont(new Font("Helvetica", Font.BOLD, 20));
+
+        // create slider and adjust settings
+        JSlider slider = new JSlider(0, 40, 20);
+        slider.setMinorTickSpacing(1);
+        slider.setMajorTickSpacing(5);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+
+        // create a panel for organizing the label and slider
+        JPanel sliderPanel = new JPanel();
+
+        // add label and slider, in left-to-right order
+        sliderPanel.add(welcomeLabel);
+        sliderPanel.add(slider);
+
+        // create the components at the bottom
+        JLabel label = new JLabel("Enter Text");
+        JTextField textField = new JTextField(10); // accepts upto 10 characters
+        JButton sendButton = new JButton("Send");
+        JButton resetButton = new JButton("Reset");
+
+        // create checkboxes
+        JCheckBox checkBox1 = new JCheckBox("Yes");
+        checkBox1.setBounds(100, 100, 50, 50);
+        JCheckBox checkBox2 = new JCheckBox("No", true);
+        checkBox2.setBounds(100, 150, 50, 50);
+
+        // create a panel for organizing the components at the bottom
+        JPanel panel = new JPanel(); // a "panel" is not visible
+
+        // add bottom components to the panel, in left-to-right order
+        panel.add(label);
+        panel.add(textField);
+        panel.add(sendButton);
+        panel.add(resetButton);
+        panel.add(checkBox1);
+        panel.add(checkBox2);
+
+        // creating a third panel to place slider and bottom panels vertically
+        // (allows two rows of UI elements to be displayed)
+        JPanel combinedPanels = new JPanel();
+        combinedPanels.setLayout(new GridLayout(2, 1));
+        combinedPanels.add(sliderPanel, BorderLayout.NORTH);
+        combinedPanels.add(panel, BorderLayout.SOUTH);
+
+        // add the menu bar to the TOP of the frame, the big white text area
+        // to the MIDDLE of the frame, and the "combinedPanels" (which has
+        // the label, slider, text box, buttons, and checkboxes) at the BOTTOM
+        add(menuBar, BorderLayout.NORTH);
+        add(textArea, BorderLayout.CENTER);
+        add(combinedPanels, BorderLayout.SOUTH);
+
+
+        //EVENT HANDLING (connects button and gui object)
+
+        sendButton.addActionListener(this);
+        resetButton.addActionListener(this);
+        // display the frame!
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e){
+        Object source = e.getSource();
+        JButton button = (JButton) source;
+        String buttonText = button.getText();
+        if(buttonText.equals("Send")){
+            welcomeLabel.setText("SEND CLICKED");
+        }else if(buttonText.equals("Reset")){
+            welcomeLabel.setText("Reset");
+        }
+        System.out.println("CLICKED");
+    }
 }
